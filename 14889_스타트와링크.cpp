0cb9c1005ac6ae10ef70a27c353ebb141cc0d63d
jeonghj <sisa339@naver.com>
2020-04-18 18:24:51 +0900

    순열을 이용하여 스타트와링크 문제풀이

--- 14889_스타트와링크.cpp
+++ 14889_스타트와링크.cpp
@@ -8,12 +8,8 @@ int go(vector <int> &d, int n) {
 	int x=0, y=0;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			if (d[i] == 0 && d[j]==0) {
-				x += a[i][j];
-			}
-			if (d[i] == 1 && d[j] == 1) {
-				y += a[i][j];
-			}
+			if (d[i] == 0 && d[j]==0) x += a[i][j];
+			if (d[i] == 1 && d[j] == 1) y += a[i][j];
 		}
 	}
 	ret = x - y;
